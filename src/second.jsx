9408c69d7ec84435a1075da7181d41dabf1915df
unknown <shivamstambe20222@gmail.com>
2022-07-20 01:36:20 -0700

    6th version

--- src/second.jsx
+++ src/second.jsx
@@ -1,12 +1,63 @@
 import React from "react";
+import { useState } from "react";
 
 function Second(){
+    
+
+    const image = '/imgs/play.png';
+    const visibility = 'visible2';
+
+
+    const [images, setImages] = useState(image);
+    const [visible, setVisible] = useState(visibility);
+
+    function Showbtn(){
+        setVisible('visible2');
+        var videoObj = document.getElementById('videoplay');
+        if (!videoObj.paused) {
+            console.log("Video is playing");
+            setImages('/imgs/pause.png');
+        } else {
+            console.log("Video is paused");
+            setImages('/imgs/play.png');
+        }
+    }
+
+    function Hidebtn(){
+        var videoObj = document.getElementById('videoplay');
+        if (!videoObj.paused) {
+            setVisible('invisible');
+        } else {
+            setVisible('visible');
+        }
+    }
+
+
+
+    function Play(){
+        // document.getElementById('videoplay'));.play()
+            var videoObj = document.getElementById('videoplay')
+        // var videoObj = videojs("videoplay");
+
+if (!videoObj.paused) {
+    console.log("Video is playing");
+    setImages('/imgs/play.png');
+    videoObj.pause();
+
+} else {
+    console.log("Video is paused");
+    setImages('/imgs/pause.png');
+    videoObj.play();
+}
+    }
     return(
         <div className="second" id="secondd">
-            <div className="video"  data-aos="slide-right" 
+            <div className="video"  data-aos="slide-right"
+                    onClick={Play} 
                     data-aos-easing="ease-in"
                     data-aos-mirror="true">
-                <video src="/video/akash.mp4" controls></video>
+                        <div className="play" ><img src={images} alt="" id="playimg" className={visible} /> </div>
+                <video src="/video/akash.mp4" onMouseEnter={Showbtn} onMouseLeave={Hidebtn}  id="videoplay"></video>
             </div>
             <div className="secinfo" data-aos="slide-left" 
                     data-aos-easing="ease-in"
